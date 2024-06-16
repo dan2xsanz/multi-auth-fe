e117931ee3043a4cb2659fe624982df6fd19967d
Dan Lester Sanz <dsanz@csyoutsourcing.com>
2024-06-16 11:44:32 +0800

    refactor common icons

--- src/app/common/icons/icons.tsx
+++ src/app/common/icons/icons.tsx
@@ -1,19 +1,146 @@
 import './icons.css'
-import React, { useState } from 'react'
+import React from 'react'
 
-interface CloseIconInterface {
+interface IconInterface {
   onClick?: () => void
+  style?: React.CSSProperties
 }
-export const CloseIcon = (props: CloseIconInterface) => {
+
+export const GoogleIcon = (props: IconInterface) => {
   const { onClick } = props
   return (
     <div className='icon-style-container' onClick={onClick}>
       <svg
         xmlns='http://www.w3.org/2000/svg'
         viewBox='0 0 512 512'
-        fill='#155263'
+        fill='black'
       >
-        <path d='M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z' />
+        <path d='M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z' />
+      </svg>
+    </div>
+  )
+}
+
+export const FacebookIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 512 512'
+        fill='black'
+      >
+        <path d='M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z' />
+      </svg>
+    </div>
+  )
+}
+
+export const GitHubIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 512 512'
+        fill='black'
+      >
+        <path d='M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z' />
+      </svg>
+    </div>
+  )
+}
+
+export const HomeIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 600 600'
+        fill='black'
+        style={{ marginTop: '0.4rem' }}
+      >
+        <path d='M543.8 287.6c17 0 32-14 32-32.1c1-9-3-17-11-24L512 185V64c0-17.7-14.3-32-32-32H448c-17.7 0-32 14.3-32 32v36.7L309.5 7c-6-5-14-7-21-7s-15 1-22 8L10 231.5c-7 7-10 15-10 24c0 18 14 32.1 32 32.1h32v69.7c-.1 .9-.1 1.8-.1 2.8V472c0 22.1 17.9 40 40 40h16c1.2 0 2.4-.1 3.6-.2c1.5 .1 3 .2 4.5 .2H160h24c22.1 0 40-17.9 40-40V448 384c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v64 24c0 22.1 17.9 40 40 40h24 32.5c1.4 0 2.8 0 4.2-.1c1.1 .1 2.2 .1 3.3 .1h16c22.1 0 40-17.9 40-40V455.8c.3-2.6 .5-5.3 .5-8.1l-.7-160.2h32z' />
+      </svg>
+    </div>
+  )
+}
+
+export const BellIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 550 550'
+        fill='black'
+        style={{ marginTop: '0.4rem' }}
+      >
+        <path d='M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z' />
+      </svg>
+    </div>
+  )
+}
+
+export const CartIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 700 700'
+        fill='black'
+        style={{ marginTop: '0.4rem', marginLeft: '-5px' }}
+      >
+        <path d='M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z' />
+      </svg>
+    </div>
+  )
+}
+
+export const GroupIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 600 600'
+        fill='black'
+        style={{ marginTop: '0.4rem' }}
+      >
+        <path d='M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z' />
+      </svg>
+    </div>
+  )
+}
+
+export const MessageIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 600 600'
+        fill='black'
+        style={{ marginTop: '0.4rem' }}
+      >
+        <path d='M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z' />
+      </svg>
+    </div>
+  )
+}
+
+export const AddIcon = (props: IconInterface) => {
+  const { onClick } = props
+  return (
+    <div className='add-icon-style-container' onClick={onClick}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 600 600'
+        fill='black'
+      >
+        <path d='M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z' />
       </svg>
     </div>
   )
