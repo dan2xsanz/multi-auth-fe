8bfe3ca37e025f9f47b3cab5f4cce5134f287194
Dan Lester Sanz <dsanz@csyoutsourcing.com>
2024-06-16 11:49:30 +0800

    add selection constant

---
+++ src/app/constant/selections/list-selections.ts
@@ -0,0 +1,18 @@
+export const productCategoryItems = [
+  { key: '1', value: 1, label: 'Shoes' },
+  { key: '2', value: 2, label: 'Jackets' },
+  { key: '3', value: 3, label: 'Hoodies & Sweatshirts' },
+  { key: '4', value: 4, label: 'Trouser & Tights' },
+  { key: '5', value: 5, label: 'Shorts' },
+  { key: '6', value: 6, label: 'Tops & Shirts' },
+  { key: '7', value: 7, label: 'Tracksuite' },
+  { key: '8', value: 8, label: 'Socks' },
+  { key: '9', value: 9, label: 'Accessories & Equipment' },
+]
+
+export const productConditionItems = [
+  { key: '1', value: 1, label: 'New' },
+  { key: '2', value: 2, label: 'Used-Like New' },
+  { key: '3', value: 3, label: 'Used-Good' },
+  { key: '4', value: 4, label: 'Used-Fair' },
+]
