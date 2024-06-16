b28948dff23b00bced0c469a8019490c4dcf5d0d
Dan Lester Sanz <dsanz@csyoutsourcing.com>
2024-06-16 11:48:55 +0800

    add snz logo

---
+++ src/app/common/logo/snz-logo.tsx
@@ -0,0 +1,10 @@
+import './snz-logo.css'
+import React from 'react'
+
+export const SnzLogo = () => {
+  return (
+    <div style={{ display: 'grid', justifyItems: 'center' }}>
+      <div className='snz-logo-style'>SNZ.</div>
+    </div>
+  )
+}
