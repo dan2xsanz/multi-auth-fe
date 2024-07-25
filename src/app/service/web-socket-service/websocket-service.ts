63a862aaa4f1a3c4ebd7599e739882a99df45893
Dan Lester Sanz <dsanz@csyoutsourcing.com>
2024-07-25 21:50:29 +0800

    add web socket implementation for notification

--- src/app/service/web-socket-service/websocket-service.ts
+++ src/app/service/web-socket-service/websocket-service.ts
@@ -1,10 +1,10 @@
-import { accountDetailStore } from '@/app/store'
 import { REQUEST_URL } from '@/properties'
 import { Client } from '@stomp/stompjs'
 import SockJS from 'sockjs-client'
 
 class WebSocketService {
   private client: Client
+  private curentLoggedInUserId: number | undefined
 
   constructor() {
     this.client = new Client({
@@ -19,17 +19,25 @@ class WebSocketService {
     this.client.onConnect = () => {
       console.log('Connected to WebSocket')
       this.client.subscribe('/topic/notifications', (message: any) => {
-        console.log('Received message: ', message.body)
-        console.log(JSON.parse(message.body)['accountMasterId'])
-        alert(`Notification: ${message.body}`)
+        // console.log('Received message: ', message.body)
+        // console.log(JSON.parse(message.body)['accountMasterId'])
+        if (
+          this.curentLoggedInUserId ===
+          JSON.parse(message.body)['accountMasterId']
+        ) {
+          alert(`Notification: ${message.body}`)
+        }
       })
     }
 
     this.client.activate()
   }
 
+  setCurrentLoggedInUser(accountMasterId: number | undefined) {
+    this.curentLoggedInUserId = accountMasterId
+  }
+
   sendAddFavoriteMessage(accountMasterId: number | undefined, message: string) {
-    console.log(accountMasterId)
     this.client.publish({
       destination: '/app/notification',
       body: JSON.stringify({
@@ -42,4 +50,3 @@ class WebSocketService {
 
 const webSocketServiceInstance = new WebSocketService()
 export default webSocketServiceInstance
-
